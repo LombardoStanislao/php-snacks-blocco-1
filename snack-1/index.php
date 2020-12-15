eaf7f20348c35f51928ecb0daa5d7ac565363af7
Stanislao <stanislaoweb@libero.it>
2020-12-15 15:18:26 +0100

    Generato array matches

---
+++ snack-1/index.php
@@ -0,0 +1,69 @@
+<!-- PHP Snack 1:
+Creare un array $matches che contiene dei sotto-array, i quali rappresentano delle partite di basket.
+ogni partita avrà le seguenti caratteristiche:
+squadra di casa
+squadra ospite
+punti fatti dalla squadra di casa
+punti fatti dalla squadra ospite
+
+L'obbiettivo è stampare a schermo tutte le partite utilizzando questo schema:
+[squadra di casa] - [squadra ospite] | [punti squadra di casa] - [punti squadra ospite]
+ad esempio:
+Olimpia Milano - Cantù | 55 - 60
+-->
+
+<?php
+
+  // Creo un array di matches
+
+    $matches = [
+      "Treviso - Cremona" [
+        "squadra di casa" => "Universo Treviso",
+        "squadra ospite" => "Cremona",
+        "punti squadra di casa" => "68",
+        "punti squadra ospite" => "75"
+      ],
+      "Cantù - Trieste" [
+        "squadra di casa" => "Cantù",
+        "squadra ospite" => "Trieste",
+        "punti squadra di casa" => "82",
+        "punti squadra ospite" => "79"
+      ],
+      "Reggiana - Fortitudo" [
+        "squadra di casa" => "Reggiana",
+        "squadra ospite" => "Fortitudo Bologna",
+        "punti squadra di casa" => "71",
+        "punti squadra ospite" => "83"
+      ],
+      "Brindisi - Pesaro" [
+        "squadra di casa" => "Brindisi",
+        "squadra ospite" => "VL Pesaro",
+        "punti squadra di casa" => "68",
+        "punti squadra ospite" => "69"
+      ],
+      "Milano - Sassari" [
+        "squadra di casa" => "Olimpia Milano",
+        "squadra ospite" => "Dinamo Sassari",
+        "punti squadra di casa" => "85",
+        "punti squadra ospite" => "68"
+      ],
+      "Virtus - Brescia" [
+        "squadra di casa" => "Virtus Bologna",
+        "squadra ospite" => "Brescia",
+        "punti squadra di casa" => "77",
+        "punti squadra ospite" => "70"
+      ],
+    ];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP | Snack-1</title>
+  </head>
+  <body>
+
+  </body>
+</html>
